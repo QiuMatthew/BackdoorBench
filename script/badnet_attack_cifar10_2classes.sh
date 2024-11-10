ba096b86902039b312bc73c37b3ec5299e0cf2f9
QiuMatthew <uzenkyu@gmail.com>
2024-11-10 21:50:45 +0900

    script: badnet_attack_cifar10_2classes.sh

--- script/badnet_attack_cifar10_2classes.sh
+++ script/badnet_attack_cifar10_2classes.sh
@@ -2,10 +2,10 @@
 #SBATCH -t 72:00:00
 #SBATCH -p qgpu
 #SBATCH --gres=gpu:tesla_a100:1
-#SBATCH --out="out/badnet_attack_cifar10.out"
+#SBATCH --out="out/badnet_attack_cifar10_2classes.out"
 
 python ./attack/badnet.py \
     --yaml_path ./config/attack/prototype/cifar10.yaml \
     --patch_mask_path ./resource/badnet/trigger_image.png \
-    --save_folder_name badnet_attack_cifar10 \
+    --save_folder_name badnet_attack_cifar10_2classes \
     --num_classes 2 \
