e4c1ced02919de72382812eccff0d432d56e2fc7
QiuMatthew <uzenkyu@gmail.com>
2024-11-10 20:37:49 +0900

    add example script for badnet

--- script/example_badnet_attack.sh
+++ script/example_badnet_attack.sh
@@ -1,2 +1,12 @@
-python ./resource/badnet/generate_white_square.py --image_size 32 --square_size 3 --distance_to_right 0 --distance_to_bottom 0 --output_path ./resource/badnet/trigger_image.png
-python ./attack/badnet.py --yaml_path ../config/attack/prototype/cifar10.yaml --patch_mask_path ../resource/badnet/trigger_image.png  --save_folder_name badnet_0_1
+#!/bin/bash
+#SBATCH -t 72:00:00
+#SBATCH -p qgpu
+#SBATCH --gres=gpu:tesla_a100:1
+#SBATCH --out="badnet_attack_efficientnet.out"
+#SBATCH --error="badnet_attack_efficientnet.err"
+
+python ./attack/badnet.py \
+    --yaml_path ../config/attack/prototype/cifar10.yaml \
+    --patch_mask_path ../resource/badnet/trigger_image.png \
+    --save_folder_name badnet_example
+
