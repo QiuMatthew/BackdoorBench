063c7406b9d6386b1678aa904e9b8896d5d29ad4
QiuMatthew <uzenkyu@gmail.com>
2024-11-11 00:24:53 +0900

    start over

--- script/badnet_attack_preactresnet_cifar10.sh
+++ script/badnet_attack_preactresnet_cifar10.sh
@@ -2,7 +2,7 @@
 #SBATCH -t 72:00:00
 #SBATCH -p qgpu
 #SBATCH --gres=gpu:tesla_a100:1
-#SBATCH --out="out/example_badnet_generate_trigger.out"
+#SBATCH --out="out/badnet_attack_preactresnet_cifar10.out"
 
 python ./resource/badnet/generate_white_square.py \
 --image_size 32 \
@@ -10,4 +10,9 @@ python ./resource/badnet/generate_white_square.py \
 --distance_to_right 0 \
 --distance_to_bottom 0 \
 --output_path ./resource/badnet/trigger_image.png
+
+python ./attack/badnet.py \
+    --yaml_path ./config/attack/prototype/cifar10.yaml \
+    --patch_mask_path ./resource/badnet/trigger_image.png \
+    --save_folder_name badnet_attack_preactresnet_cifar10
 
--- script/badnet_attack_preactresnet_cifar10_2classes.sh
+++ script/badnet_attack_preactresnet_cifar10_2classes.sh
@@ -2,7 +2,7 @@
 #SBATCH -t 72:00:00
 #SBATCH -p qgpu
 #SBATCH --gres=gpu:tesla_a100:1
-#SBATCH --out="out/example_badnet_attack.out"
+#SBATCH --out="out/badnet_attack_preactresnet_cifar10_2classes.out"
 
 python ./resource/badnet/generate_white_square.py \
 --image_size 32 \
@@ -11,9 +11,8 @@ python ./resource/badnet/generate_white_square.py \
 --distance_to_bottom 0 \
 --output_path ./resource/badnet/trigger_image.png
 
-
 python ./attack/badnet.py \
     --yaml_path ./config/attack/prototype/cifar10.yaml \
     --patch_mask_path ./resource/badnet/trigger_image.png \
-    --save_folder_name badnet_example
-
+    --save_folder_name badnet_attack_preactresnet_cifar10_2classes \
+    --dataset cifar10_2classes \
