87a48c54c1e38b24ae97308202a3ecdef85dee19
QiuMatthew <uzenkyu@gmail.com>
2024-11-10 23:48:42 +0900

    try mnist

---
+++ script/badnet_attack_mnist.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#SBATCH -t 72:00:00
+#SBATCH -p qgpu
+#SBATCH --gres=gpu:tesla_a100:1
+#SBATCH --out="out/badnet_attack_mnist.out"
+
+python ./attack/badnet.py \
+    --yaml_path ./config/attack/prototype/mnist.yaml \
+    --patch_mask_path ./resource/badnet/trigger_image.png \
+    --save_folder_name badnet_attack_mnist \
+    --dataset mnist \
