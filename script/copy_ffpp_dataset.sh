fc69521dc88af950aed806fd91822aaf66a11b80
QiuMatthew <uzenkyu@gmail.com>
2024-11-16 23:32:24 +0900

    copy dataset script

---
+++ script/copy_ffpp_dataset.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+#SBATCH -p qcpu
+#SBATCH --out="out/copy_ffpp_dataset.out"
+
+cp -r ~/BackdoorBenchIJCB/data/ffpp ~/BackdoorBench/data/
+mv ~/BackdoorBench/data/ffpp ~/BackdoorBench/data/ffpp_binary
