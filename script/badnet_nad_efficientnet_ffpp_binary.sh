3a351be86fb26c3d0725d4008b170fbdda289f6b
QiuMatthew <uzenkyu@gmail.com>
2024-11-17 20:58:46 +0900

    new script: badnet nad/ibau efficientnet ffpp binary

---
+++ script/badnet_nad_efficientnet_ffpp_binary.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#SBATCH -t 72:00:00
+#SBATCH -p qgpu
+#SBATCH --gres=gpu:tesla_a100:1
+#SBATCH --out="out/badnet_nad_efficientnet_ffpp_binary.out"
+
+python ./defense/nad.py \
+    --yaml_path ./config/defense/nad/cifar10.yaml \
+    --model efficientnet_b3 \
+    --dataset ffpp_binary \
+    --result_file badnet_attack_efficientnet_ffpp_binary \
