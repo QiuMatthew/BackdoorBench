a0e25267a6f613d100dbd1e18a5e86b89887984b
QiuMatthew <uzenkyu@gmail.com>
2024-11-17 17:12:38 +0900

    new script: generate ffpp dataset with 3/4/5 classes

---
+++ script/generate_ffpp_with_345_classes.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#SBATCH -p qcpu
+#SBATCH --out="out/generate_ffpp_with_345_classes.out"
+
+cp -r ~/BackdoorBenchIJCB/data/ffpp_multiclass ~/BackdoorBench/data/
+mv ~/BackdoorBench/data/ffpp_multiclass ~/BackdoorBench/data/ffpp_5classes
+rm -r ~/BackdoorBench/data/ffpp_5classes/5_faceshifter
+cp -r ~/BackdoorBench/data/ffpp_5classes ~/BackdoorBench/data/ffpp_4classes
+rm -r ~/BackdoorBench/data/ffpp_4classes/4_neuraltextures
+cp -r ~/BackdoorBench/data/ffpp_4classes ~/BackdoorBench/data/ffpp_3classes
+rm -r ~/BackdoorBench/data/ffpp_3classes/3_faceswap
