85c8864e726c524bf4646e5030a44617cb38c27e
QiuMatthew <uzenkyu@gmail.com>
2024-11-18 18:49:48 +0900

    new script: badnet abl/nad/ibau defense on ffpp_3/4/5/classes and ffpp_multiclass(6classes)

---
+++ script/badnet_ibau_efficientnet_ffpp_3classes.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#SBATCH -t 72:00:00
+#SBATCH -p qgpu
+#SBATCH --gres=gpu:tesla_a100:1
+#SBATCH --out="out/badnet_ibau_efficientnet_ffpp_3classes.out"
+
+python ./defense/i-bau.py \
+    --yaml_path ./config/defense/i-bau/cifar10.yaml \
+    --model efficientnet_b3 \
+    --dataset ffpp_3classes \
+    --result_file badnet_attack_efficientnet_ffpp_3classes \
