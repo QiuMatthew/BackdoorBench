baafb796919dbe5ea0faf001b1e1287def93a2ff
QiuMatthew <uzenkyu@gmail.com>
2024-12-23 01:31:30 +0900

    new script: badnet attack with merged fake classes metric

---
+++ script/badnet_attack_efficientnet_ffpp_2classes_to_binary.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#SBATCH -t 72:00:00
+#SBATCH -p qgpu
+#SBATCH --gres=gpu:tesla_a100:1
+#SBATCH --out="out/badnet_attack_efficientnet_ffpp_2classes_to_binary.out"
+
+python ./resource/badnet/generate_white_square.py \
+--image_size 64 \
+--square_size 3 \
+--distance_to_right 0 \
+--distance_to_bottom 0 \
+--output_path ./resource/badnet/trigger_image.png
+
+python ./attack/badnet.py \
+    --yaml_path ./config/attack/prototype/cifar10.yaml \
+    --model efficientnet_b3 \
+    --dataset ffpp_binary \
+    --patch_mask_path ./resource/badnet/trigger_image.png \
+    --save_folder_name badnet_attack_efficientnet_ffpp_2classes_to_binary \
+
