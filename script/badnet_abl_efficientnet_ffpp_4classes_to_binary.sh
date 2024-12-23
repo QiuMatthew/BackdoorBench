e019ddd1647918828afb5a59e117a864c26da1b7
QiuMatthew <uzenkyu@gmail.com>
2024-12-24 01:17:57 +0900

    new script: defenses with merged fake classes metric

---
+++ script/badnet_abl_efficientnet_ffpp_4classes_to_binary.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#SBATCH -t 72:00:00
+#SBATCH -p qgpu
+#SBATCH --gres=gpu:tesla_a100:1
+#SBATCH --out="out/badnet_abl_efficientnet_ffpp_4classes_to_binary.out"
+
+python ./defense/abl.py \
+    --yaml_path ./config/defense/abl/cifar10.yaml \
+    --model efficientnet_b3 \
+    --dataset ffpp_4classes \
+    --result_file badnet_attack_efficientnet_ffpp_4classes_to_binary \
---
+++ script/badnet_nad_efficientnet_ffpp_3classes_to_binary.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#SBATCH -t 72:00:00
+#SBATCH -p qgpu
+#SBATCH --gres=gpu:tesla_a100:1
+#SBATCH --out="out/badnet_nad_efficientnet_ffpp_3classs_to_binary.out"
+
+python ./defense/nad.py \
+    --yaml_path ./config/defense/nad/cifar10.yaml \
+    --model efficientnet_b3 \
+    --dataset ffpp_3classes \
+    --result_file badnet_attack_efficientnet_ffpp_3classes_to_binary \
