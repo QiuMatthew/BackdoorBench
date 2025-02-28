3aeef89c373ad42ce49a6f0ec5a72587dcfacbfe
QiuMatthew <q_masio@outlook.com>
2025-02-28 14:32:29 +0900

    test beta 3

--- script/nad/badnet_nad_efficientnet_ffpp_6classes_x_x_10.sh
+++ script/nad/badnet_nad_efficientnet_ffpp_6classes_x_x_10.sh
@@ -2,11 +2,11 @@
 #SBATCH -t 72:00:00
 #SBATCH -p qgpu
 #SBATCH --gres=gpu:tesla_a100:1
-#SBATCH --out="out/badnet_nad_efficientnet_ffpp_multiclass.out"
+#SBATCH --out="out/nad/badnet_nad_efficientnet_ffpp_6classes_x_x_10.out"
 
 python ./defense/nad.py \
     --yaml_path ./config/defense/nad/cifar10.yaml \
     --model efficientnet_b3 \
-    --dataset ffpp_multiclass \
-    --result_file badnet_attack_efficientnet_ffpp_multiclass \
-    --beta3 0.1 \
+    --dataset ffpp_6classes \
+    --result_file badnet_attack_efficientnet_ffpp_6classes \
+    --beta3 10 \
